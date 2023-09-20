e299063a1d0a936172ca33d79f73379a7185ee6f
Or Hasson <hassonor@gmail.com>
2023-09-20 12:37:49 +0300

    improve explain of longest common prefix question

--- interview-questions-python/strings/longest_common_prefix.py
+++ interview-questions-python/strings/longest_common_prefix.py
@@ -1,34 +1,37 @@
 """
 Explanation:
-zip function allows me to loop through each string character simultaneously, 
-rather than using indexes to iterate through by unpacking the array with the wildcard.
-So for each loop, 'i' would be a tuple of the 0th character, then the 1st character etc
-The set function basically removes duplicates 
-and tells me how many unique characters there are at that index across all strings.
-If it is not equal to 1, then it implies that at least one of the characters that a given index are not the same, 
-and therefore should break and return whatever the previous 'bestString' was.
+The zip function allows for simultaneous iteration through each string's character by unpacking the list of strings with the wildcard.
+So, in each loop, 'i' is a tuple containing the characters from all strings at the same index (0th character in the first loop, 1st character in the second loop, etc.).
+The set function removes duplicates and indicates how many unique characters are at that index across all strings.
+If the number of unique characters isn't 1, at least one of the strings has a different character at that index.
+In such a case, we break out of the loop and return the longest common prefix found up to that point.
 
-To clarify with an example, if two string were passed, 'year' and 'yes', the 'i' value will appear as follows:
+Example:
+Consider two strings: 'year' and 'yes'.
+The value of 'i' in each iteration will be:
 First iteration: i = ('y', 'y')
 Second iteration: i = ('e', 'e')
 Third iteration: i = ('a', 's')
-The program will break after the third since there is more than one unique character found, returning 
-the longest common prefix which in this case is 'ye' 
+The loop breaks in the third iteration since there's more than one unique character.
+The function will thus return the longest common prefix found up to the second iteration, which is 'ye'.
 """
 
 
 def longest_common_prefix(strs):
     longest_common_prefix_string = ''
 
+    # Iterate over characters of all strings simultaneously
     for i in zip(*strs):
+        # Check if all characters in the current position are the same
         if len(set(i)) != 1:
             break
+        # Add the common character to the prefix string
         longest_common_prefix_string += i[0]
 
     return longest_common_prefix_string
 
 
+# Test the function
 print(longest_common_prefix(
     ["orhasson123123123jklafshfdjhjsdgkf", "orhasson1afsklojaslkasjf1243", "orhasson2sf;alkl;fask"]))
-
 # Output -> orhasson
