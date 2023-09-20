0aac0e6a27a48d901ae08ffc5557a86cbc150626
Or Hasson <hassonor@gmail.com>
2023-09-20 18:46:17 +0300

    add namedtuples example

---
+++ advanced-python-working-with-data/collection_classes/INFO.md
@@ -0,0 +1,13 @@
+## Advanced Collections
+
+* `list` - Mutable sequence of values
+* `tuple` - Fixed sequence of values
+* `set` - Sequence of distinct values
+* `dictionary` - Collection of key-value pairs
+
+### Advanced Collections (import collections)
+
+* `namedtuple` - Tuple with names fields
+* `defaultdict` - Dictionary that supplies default values
+* `Counter` - Counts distinct values
+* `deque` - Double-ended list object
---
+++ advanced-python-working-with-data/collection_classes/namedtuple.py
@@ -0,0 +1,24 @@
+# Importing the `collections` module which provides utility functions/classes to work with Python collections
+import collections
+
+# Using the `namedtuple` function to create a custom tuple class named "Point".
+# This custom tuple will have named fields "x" and "y".
+Point = collections.namedtuple("Point", "x y")
+
+# Creating instances of our custom tuple "Point"
+p1 = Point(10, 20)
+p2 = Point(30, 40)
+
+# Printing the values of our points
+print(p1, p2)
+
+# Accessing the individual fields of the namedtuples using dot notation
+print(p1.x, p1.y)  # This will print the x and y values of p1
+print(p2.x, p2.y)  # This will print the x and y values of p2
+
+# Using the `_replace()` method of namedtuples to create a new Point with modified field values.
+# Here, we're updating the x value of p1.
+p1 = p1._replace(x=100)
+
+# Printing the updated p1 value
+print(p1)
