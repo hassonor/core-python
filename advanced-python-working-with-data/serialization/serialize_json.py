5ba1d46a8b767e6fb472b5ee4716505f77b8a108
Or Hasson <hassonor@gmail.com>
2023-09-21 11:54:35 +0300

    add serialize json demo

--- advanced-python-working-with-data/serialization/serialize_json.py
+++ advanced-python-working-with-data/serialization/serialize_json.py
@@ -8,3 +8,23 @@ with open("30DayQuakes.json", "r") as date_file:
 def is_big(x):
     mag = x["properties"]["mag"]
     return mag is not None and mag > 6
+
+
+def simple_quake(q):
+    return {
+        "place": q["properties"]["place"],
+        "mag": q["properties"]["mag"],
+        "link": q["properties"]["url"],
+        "date": str(datetime.date.fromtimestamp(int(q["properties"]["time"] / 1000)))
+    }
+
+
+large_quakes = list(filter(is_big, data["features"]))
+
+large_quakes = list(map(simple_quake, large_quakes))
+
+large_quakes_str = json.dumps(large_quakes, sort_keys=True, indent=4)
+print(large_quakes_str)
+
+with open("large_quakes.json", "w") as outfile:
+    json.dump(large_quakes, outfile, sort_keys=True, indent=4)
