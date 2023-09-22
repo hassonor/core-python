328f27a56cac99eccd894d0926968ebbb60b061d
Or Hasson <hassonor@gmail.com>
2023-09-22 15:51:30 +0300

    add challenge solution

--- advanced-python-working-with-databases/mysql-sqlalchemy-workspace/main.py
+++ advanced-python-working-with-databases/mysql-sqlalchemy-workspace/main.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, desc, func
 from sqlalchemy.orm import Session
 from config import DATABASE_URL
-from models import Base, Project, Task
+from models import Base, Project, Task, Sale
 
 engine = create_engine(DATABASE_URL, echo=True)
 Base.metadata.create_all(engine)
@@ -25,6 +25,30 @@ def print_tasks_for_project(_session, project_title):
         print(task)
 
 
+def insert_sample_data(_session):
+    data = [
+        Sale(order_num=1, order_type="Online", cust_name="John", prod_category="Electronics", prod_number="E123",
+             prod_name="Laptop", quantity=2, price=1000, discount=50, order_total=1950),
+        Sale(order_num=2, order_type="Online", cust_name="Doe", prod_category="Clothing", prod_number="C456",
+             prod_name="T-Shirt", quantity=5, price=20, discount=5, order_total=95),
+        Sale(order_num=3, order_type="Online", cust_name="Smith", prod_category="Books", prod_number="B789",
+             prod_name="Novel", quantity=3, price=15, discount=0, order_total=45),
+        Sale(order_num=4, order_type="Online", cust_name="Davis", prod_category="Electronics", prod_number="E101",
+             prod_name="Headphones", quantity=1, price=150, discount=10, order_total=140),
+        Sale(order_num=5, order_type="Online", cust_name="White", prod_category="Home", prod_number="H202",
+             prod_name="Mixer", quantity=1, price=100, discount=0, order_total=100)
+    ]
+    _session.bulk_save_objects(data)
+    _session.commit()
+
+
+def most_expensive_order(_session):
+    return _session.query(Sale).order_by(desc(Sale.order_total)).first()
+    # Another way
+    # max_total = _session.query(func.max(Sale.order_total)).scalar()
+    # return _session.query(Sale).filter(Sale.order_total == max_total).first()
+
+
 if __name__ == '__main__':
     with Session(engine) as session:
         # Uncomment below lines to add data
@@ -34,4 +58,11 @@ if __name__ == '__main__':
         #     'Organize winter clothes'
         # ])
 
-        print_tasks_for_project(session, 'Organize closet')
+        # print_tasks_for_project(session, 'Organize closet')
+        # insert_sample_data(session)
+        expensive_order = most_expensive_order(session)
+        if expensive_order:
+            print(
+                f"The most expensive order was made by {expensive_order.cust_name} with a total of {expensive_order.order_total}.")
+        else:
+            print("No orders found in the database.")
--- advanced-python-working-with-databases/mysql-sqlalchemy-workspace/models.py
+++ advanced-python-working-with-databases/mysql-sqlalchemy-workspace/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from sqlalchemy.orm import registry, relationship
 
 mapper_registry = registry()
@@ -25,3 +25,24 @@ class Task(Base):
 
     def __repr__(self):
         return f"<Task(description='{self.description}')>"
+
+
+class Sale(Base):
+    __tablename__ = 'sales'
+    order_num = Column(Integer, primary_key=True)
+    order_type = Column(String(length=255))
+    cust_name = Column(String(length=255))
+    prod_category = Column(String(length=255))
+    prod_number = Column(String, nullable=False)
+    prod_name = Column(String(length=255))
+    quantity = Column(Integer)
+    price = Column(Float)
+    discount = Column(Float)
+    order_total = Column(Integer)
+
+    def __repr__(self):
+        return (f"<Sale(order_num={self.order_num}, order_type={self.order_type}, "
+                f"cust_name={self.cust_name}, prod_category={self.prod_category},"
+                f" prod_number={self.prod_number}, prod_name={self.prod_name}, "
+                f"quantity={self.quantity}, price={self.price}, discount={self.discount}, "
+                f"order_total={self.order_total})>")
