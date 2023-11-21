9f4779336262356805caf4417bd8df740db2ed01
Or Hasson <hassonor@gmail.com>
2023-11-21 10:56:38 +0200

    add first example for defensive programming etc..

---
+++ interview-questions-python/defensive_programming/ex_1.py
@@ -0,0 +1,12 @@
+import re
+
+factor = 1 + 17 / 100
+
+basic = input("Please enter the original price: ")
+while basic != "quit":
+    if re.search("^-?[0-9]+.?[0-9]?$", basic):
+        sum = float(basic) * factor
+        print("Sum with VAT is ", sum)
+    else:
+        print("Non-numeric value, please try again")
+    basic = input("Please enter the original price: ")
