bc5749f78bdc74ee4e7a0ffa2c760804bb470433
Or Hasson <hassonor@gmail.com>
2024-01-04 22:25:31 +0200

    fix regular expression

--- interview-questions-python/defensive_programming/ex_1.py
+++ interview-questions-python/defensive_programming/ex_1.py
@@ -4,7 +4,7 @@ factor = 1 + 17 / 100
 
 basic = input("Please enter the original price: ")
 while basic != "quit":
-    if re.search("^-?[0-9]+.?[0-9]+?$", basic):
+    if re.search("^-?[0-9]*(\.[0-9]+)?$", basic):
         sum = float(basic) * factor
         print("Sum with VAT is ", sum)
     else:
