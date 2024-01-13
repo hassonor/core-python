b2837dab7a2b2c26bd72f2b4ec50240877ca94ad
Or Hasson <hassonor@gmail.com>
2024-01-13 18:45:34 +0200

    add more class examples

--- interview-questions-python/defensive_programming/class_examples.py
+++ interview-questions-python/defensive_programming/class_examples.py
@@ -39,3 +39,62 @@ print(same_letter_count)
 # Oneline Way2
 same_letter_count = sum(1 for ele in sentence.split(" ") if len(ele) >= 1 and ele[0] == ele[-1])
 print(same_letter_count)
+
+# Range Example
+a, b = 0, 1
+
+# Fibonacci
+for num in range(8):
+    print(a)
+    a, b = b, a + b
+
+for num in range(10, -1, -1):
+    print(num)
+
+# Sets
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)  # show that duplicates have been removed
+print('orange' in basket)  # fast membership testing
+print('crabgrass' in basket)
+
+# Demonstrate set operations on unique letters from two words
+a = set('abracadabra')
+b = set('alacazam')
+print(a)  # unique letters in 'a'
+print(a - b)  # letters in 'a' but not in 'b'
+print(a | b)  # letters in 'a' or 'b' or both
+print(a & b)  # letters in both 'a' and 'b'
+print(a ^ b)  # letters in a or b but not in both
+
+# Dictionary
+"""
+clear() - מוחק את תוכן המילון
+copy() - מעתיק את המילון 
+fromkeys() - יוצר מילון חדש עם סדרת מפתחות וערך נתון
+get() - מחזיר את הערך שמתאים למפתח מסוים
+items() - מייצר רשימה של 
+     tuples 
+    מהצורה key-value
+
+keys() - מחזיר רשימה של כל מפתחות המילון
+values() - מחזיר רשימה של כל הערכים במילון
+pop() - מוחק מהמילון את הערך עם המפתח הנתון
+"""
+thisdict = {
+    "Brand": "Ford",
+    "Model": "Mustang",
+    "Year": 1964
+}
+
+print(thisdict["Year"])
+print(thisdict.get("Year"))  # Here if the key is not exist return None
+thisdict["Year"] = 1980
+print(thisdict["Year"])
+
+default_value = "Unknown"
+new_dict = dict.fromkeys(thisdict, default_value)
+print(new_dict)
+
+print(thisdict.items())
+print(thisdict.keys())
+print(thisdict.values())
