ffa68d6d8ac6bff9f8621749fd65b174f25f0aaa
Or Hasson <hassonor@gmail.com>
2024-01-15 14:57:05 +0200

    test2

--- interview-questions-python/defensive_programming/basic/list.py
+++ interview-questions-python/defensive_programming/basic/list.py
@@ -81,3 +81,18 @@ print(my_list)
 # A shorter way of previous example:
 my_list2 = [word.upper() for word in line.split() if 'o' in word]
 print(my_list2)
+
+# Create a sample collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Strategy:  Create a new collection
+active_users = {}
+
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
