0f9f5329a5564549952b7ff957bf72cc2c2af122
Or Hasson <hassonor@gmail.com>
2024-01-24 16:49:20 +0200

    add error handling client and server socket demo

--- interview-questions-python/defensive_programming/socket/client.py
+++ interview-questions-python/defensive_programming/socket/client.py
@@ -4,9 +4,14 @@ HOST = '127.0.0.1'  # The server's hostnames or IP address
 PORT = 65432  # The port used by the server
 
 # socket.AF_INET -> Ipv4 , socket.SOCK_STREAM -> TCP, s-> context manager
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))  # The inner "()" represents a tuple
-    s.sendall(b'Hello,world')
-    data = s.recv(1024)
-    
-print('Received', repr(data))
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))  # The inner "()" represents a tuple
+        s.sendall(b'Hello, world')  # Send a message
+        data = s.recv(1024)  # Receive a response
+
+        print('Received', repr(data))
+except ConnectionRefusedError:
+    print(f"Connection failed. Is the server running on {HOST}:{PORT}?")
+except socket.error as e:
+    print(f"Socket error: {e}")
